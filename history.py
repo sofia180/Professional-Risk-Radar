c7398e76757fdda82fb169a565cd7826d58641e3
radyyynska <69909693+sofia180@users.noreply.github.com>
2026-01-11 16:34:16 +0100

    Implement save_portfolio_history function
    Add function to save portfolio history to a JSON file

--- history.py
+++ history.py
@@ -1,1 +1,11 @@
+import os, json
 
+def save_portfolio_history(metrics_dict, filename="history.json"):
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            history = json.load(f)
+    else:
+        history = []
+    history.append(metrics_dict)
+    with open(filename, "w") as f:
+        json.dump(history, f)
