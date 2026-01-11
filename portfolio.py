88abb2c4c4643666e2c6248981cbcff6e324a23e
radyyynska <69909693+sofia180@users.noreply.github.com>
2026-01-11 17:01:35 +0100

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -3,7 +3,10 @@ import numpy as np
 
 def portfolio_risk(df_numeric):
     """
-    Расчет основных метрик портфеля: среднее, стандартное отклонение, VaR 95%.
+    Расчет основных метрик портфеля:
+    - Среднее
+    - Стандартное отклонение
+    - VaR 95%
     """
     mean = df_numeric.mean().mean()
     std = df_numeric.std().mean()
@@ -16,14 +19,15 @@ def portfolio_risk(df_numeric):
 
 def correlation_matrix(df_numeric):
     """
-    Корреляционная матрица портфеля.
+    Корреляционная матрица портфеля
     """
     return df_numeric.corr()
 
 def stress_scenario(df_numeric, shock_percent):
     """
-    Симуляция стресс-сценария: уменьшение всех значений на shock_percent.
+    Симуляция стресс-сценария:
+    - Уменьшение всех значений на shock_percent
     """
-    factor = 1 - shock_percent/100
+    factor = 1 - shock_percent / 100
     stressed_df = df_numeric * factor
     return stressed_df
